65d1681f16eb1b967b86130714fdcbaa8cd04059
SantosMatute <santos22matute@gmail.com>
2023-12-22 15:48:45 -0600

    bases: modulo 4

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import {NgModule} from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+import { AppComponent } from "./app.component";
+
+
+import { counterModule } from "./counter/app.module";
+import { Heroesmodule } from "./heroes/heroes.module";
+
+@NgModule({
+  declarations:[
+    AppComponent
+
+  ],
+  imports:[
+    BrowserModule,
+    counterModule,
+    Heroesmodule
+  ],
+  providers:[],
+  bootstrap:[AppComponent],
+
+
+})
+
+export class AppModule{}
